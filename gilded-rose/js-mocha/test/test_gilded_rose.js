dee2ec7eaf8f3930da398b7311573bc7737b0aee
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-24 09:50:28 +0200

    gilded-rose: add unit test to cover update quality of regular item with passed sell date for 1 day

--- gilded-rose/js-mocha/test/test_gilded_rose.js
+++ gilded-rose/js-mocha/test/test_gilded_rose.js
@@ -13,5 +13,16 @@ describe('Gilded Rose', () => {
         quality: 19
       })
     })
+
+    it('should update quality of regular item with passed sell date for 1 day', () => {
+      const gildedRose = new Shop([new Item('Dexterity Vest', 0, 20)])
+      const items = gildedRose.updateQuality()
+
+      expect(items[0]).to.eql({
+        name: 'Dexterity Vest',
+        sellIn: -1,
+        quality: 18
+      })
+    })
   })
 })
