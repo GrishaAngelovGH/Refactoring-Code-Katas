d1f4c04b8ae139c3701f99cea02e0d962dcc47d2
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-26 10:55:27 +0200

    gilded-rose: implement updateInventory method as part of Shop

--- gilded-rose/js-mocha/src/gilded_rose.js
+++ gilded-rose/js-mocha/src/gilded_rose.js
@@ -9,53 +9,45 @@ class Item {
 class Shop {
   constructor(items = []) {
     this.items = items
+    this.specialItems = {
+      'Aged Brie': item => {
+        item.sellIn = item.sellIn - 1
+
+        item.quality = item.quality < 50 ? item.quality + 1 : item.quality
+      },
+      'Sulfuras, Hand of Ragnaros': () => { },
+      'Backstage passes to a TAFKAL80ETC concert': item => {
+        item.sellIn = item.sellIn - 1
+
+        item.quality = item.sellIn > 10 ? item.quality + 1 : item.quality
+
+        item.quality = item.sellIn < 11 ? item.quality + 2 : item.quality
+
+        item.quality = item.sellIn < 6 ? item.quality + 1 : item.quality
+
+        item.quality = item.sellIn < 0 ? 0 : item.quality
+
+        item.quality = item.quality > 50 ? 50 : item.quality
+      }
+    }
+
+    this.updateInventory = item => {
+      const isSpecialItem = this.specialItems.hasOwnProperty(item.name)
+
+      if (!isSpecialItem) {
+        item.sellIn = item.sellIn - 1
+
+        item.quality = item.quality > 0 ?
+          (item.sellIn > 0 ? item.quality - 1 : item.quality - 2) : item.quality
+      }
+
+      isSpecialItem && this.specialItems[item.name](item)
+    }
   }
 
   updateQuality() {
     this.items.forEach(item => {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn = item.sellIn - 1
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality = item.quality - 1
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
-      }
+      this.updateInventory(item)
     })
 
     return this.items
