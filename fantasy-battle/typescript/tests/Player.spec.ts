f183e69b8889a725eb4d160341f6f8c8c9a63023
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-27 09:30:12 +0200

    fantasy-battle: add unit test to cover damage calculation

--- fantasy-battle/typescript/tests/Player.spec.ts
+++ fantasy-battle/typescript/tests/Player.spec.ts
@@ -1,19 +1,31 @@
-import { Inventory } from '../src/Inventory';
-import { Stats } from '../src/Stats';
-import { SimpleEnemy } from '../src/SimpleEnemy';
-import { Player } from '../src/Player';
-import { Damage } from '../src/Damage';
+import { Inventory } from '../src/Inventory'
+import { Stats } from '../src/Stats'
+import { SimpleEnemy } from '../src/SimpleEnemy'
+import { Player } from '../src/Player'
+import { Damage } from '../src/Damage'
+import { BasicItem } from '../src/BasicItem'
+import { Equipment } from '../src/Equipment'
+import { SimpleArmor } from '../src/SimpleArmor'
+import { BasicBuff } from '../src/BasicBuff'
 
 describe('Player', () => {
+  it('calculates damage', () => {
+    const equipment = new Equipment(
+      new BasicItem('round shield', 0, 1.4),
+      new BasicItem('excalibur', 20, 1.5),
+      new BasicItem('helmet of swiftness', 0, 1.2),
+      new BasicItem('ten league boots', 0, 0.1),
+      new BasicItem('breastplate of steel', 0, 1.4)
+    )
 
-    // TODO: test is not finished!
+    const inventory = new Inventory(equipment)
+    const stats = new Stats(1)
+    const enemy = new SimpleEnemy(
+      new SimpleArmor(5),
+      [new BasicBuff(1, 1)]
+    )
 
-    it('calculates damage', () => {
-        const inventory = new Inventory(null!!);
-        const stats = new Stats(0);
-        const enemy = new SimpleEnemy(null!!, null!!);
-
-        const damage: Damage = new Player(inventory, stats).calculateDamage(enemy);
-        expect(damage.amount).toBe(10);
-    });
+    const damage: Damage = new Player(inventory, stats).calculateDamage(enemy)
+    expect(damage.amount).toBe(104)
+  })
 })
