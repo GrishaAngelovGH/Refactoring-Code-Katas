4987246d72c00bb9b825d414750c3b1050443c7f
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-28 09:30:11 +0200

    tennis: extract score logic into separate functions

--- tennis/javascript-jest/tennis1.js
+++ tennis/javascript-jest/tennis1.js
@@ -13,27 +13,42 @@ const points = {
   3: 'Forty'
 }
 
+const getScoreOrDeuce = score => scores[score] || 'Deuce'
+
+const calculateScoreGTE4 = (score, m_score1, m_score2) => {
+  var minusResult = m_score1 - m_score2
+  if (minusResult === 1) { score = 'Advantage player1' }
+  else if (minusResult === -1) { score = 'Advantage player2' }
+  else if (minusResult >= 2) { score = 'Win for player1' }
+  else { score = 'Win for player2' }
+  return score
+}
+
+const calculateScoreLT3 = (score, m_score1, m_score2, tempScore) => {
+  for (var i = 1; i < 3; i++) {
+    if (i === 1) { tempScore = m_score1 }
+    else {
+      score += '-'
+      tempScore = m_score2
+    }
+    score += points[tempScore]
+  }
+
+  return score
+}
+
 function getScore(m_score1, m_score2) {
   let score = ''
   let tempScore = 0
+
   if (m_score1 === m_score2) {
-    score = scores[m_score1] || 'Deuce'
+    score = getScoreOrDeuce(m_score1)
   } else if (m_score1 >= 4 || m_score2 >= 4) {
-    var minusResult = m_score1 - m_score2
-    if (minusResult === 1) { score = 'Advantage player1' }
-    else if (minusResult === -1) { score = 'Advantage player2' }
-    else if (minusResult >= 2) { score = 'Win for player1' }
-    else { score = 'Win for player2' }
+    score = calculateScoreGTE4(score, m_score1, m_score2)
   } else {
-    for (var i = 1; i < 3; i++) {
-      if (i === 1) { tempScore = m_score1 }
-      else {
-        score += '-'
-        tempScore = m_score2
-      }
-      score += points[tempScore]
-    }
+    score = calculateScoreLT3(score, m_score1, m_score2, tempScore)
   }
+
   return score
 }
 
