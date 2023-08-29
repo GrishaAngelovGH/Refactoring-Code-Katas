c04c8fa5261d39cd47739fc0d60fc0a07c289126
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-29 09:15:34 +0200

    tennis2: rename arguments of getScore() function

--- tennis/javascript-jest/tennis2.js
+++ tennis/javascript-jest/tennis2.js
@@ -1,48 +1,48 @@
 'use strict'
 
-function getScore(P1point, P2point) {
+const getScore = (p1Point, p2Point) => {
   var score = ''
 
-  if (P1point === P2point && P1point < 3) {
-    if (P1point === 0) {
+  if (p1Point === p2Point && p1Point < 3) {
+    if (p1Point === 0) {
       score = 'Love'
     }
-    if (P1point === 1) {
+    if (p1Point === 1) {
       score = 'Fifteen'
     }
-    if (P1point === 2) {
+    if (p1Point === 2) {
       score = 'Thirty'
     }
     score += '-All'
   }
-  if (P1point === P2point && P1point > 2) {
+  if (p1Point === p2Point && p1Point > 2) {
     score = 'Deuce'
   }
 
   var P1res
   var P2res
-  if (P1point > 0 && P2point === 0) {
-    if (P1point === 1) {
+  if (p1Point > 0 && p2Point === 0) {
+    if (p1Point === 1) {
       P1res = 'Fifteen'
     }
-    if (P1point === 2) {
+    if (p1Point === 2) {
       P1res = 'Thirty'
     }
-    if (P1point === 3) {
+    if (p1Point === 3) {
       P1res = 'Forty'
     }
 
     P2res = 'Love'
     score = P1res + '-' + P2res
   }
-  if (P2point > 0 && P1point === 0) {
-    if (P2point === 1) {
+  if (p2Point > 0 && p1Point === 0) {
+    if (p2Point === 1) {
       P2res = 'Fifteen'
     }
-    if (P2point === 2) {
+    if (p2Point === 2) {
       P2res = 'Thirty'
     }
-    if (P2point === 3) {
+    if (p2Point === 3) {
       P2res = 'Forty'
     }
 
@@ -50,49 +50,49 @@ function getScore(P1point, P2point) {
     score = P1res + '-' + P2res
   }
 
-  if (P1point > P2point && P1point < 4) {
-    if (P1point === 2) {
+  if (p1Point > p2Point && p1Point < 4) {
+    if (p1Point === 2) {
       P1res = 'Thirty'
     }
-    if (P1point === 3) {
+    if (p1Point === 3) {
       P1res = 'Forty'
     }
-    if (P2point === 1) {
+    if (p2Point === 1) {
       P2res = 'Fifteen'
     }
-    if (P2point === 2) {
+    if (p2Point === 2) {
       P2res = 'Thirty'
     }
     score = P1res + '-' + P2res
   }
-  if (P2point > P1point && P2point < 4) {
-    if (P2point === 2) {
+  if (p2Point > p1Point && p2Point < 4) {
+    if (p2Point === 2) {
       P2res = 'Thirty'
     }
-    if (P2point === 3) {
+    if (p2Point === 3) {
       P2res = 'Forty'
     }
-    if (P1point === 1) {
+    if (p1Point === 1) {
       P1res = 'Fifteen'
     }
-    if (P1point === 2) {
+    if (p1Point === 2) {
       P1res = 'Thirty'
     }
     score = P1res + '-' + P2res
   }
 
-  if (P1point > P2point && P2point >= 3) {
+  if (p1Point > p2Point && p2Point >= 3) {
     score = 'Advantage player1'
   }
 
-  if (P2point > P1point && P1point >= 3) {
+  if (p2Point > p1Point && p1Point >= 3) {
     score = 'Advantage player2'
   }
 
-  if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
+  if (p1Point >= 4 && p2Point >= 0 && (p1Point - p2Point) >= 2) {
     score = 'Win for player1'
   }
-  if (P2point >= 4 && P1point >= 0 && (P2point - P1point) >= 2) {
+  if (p2Point >= 4 && p1Point >= 0 && (p2Point - p1Point) >= 2) {
     score = 'Win for player2'
   }
   return score
