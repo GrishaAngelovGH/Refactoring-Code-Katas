76114259183676795cb622b94e9095ff2e60e27d
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-29 09:41:27 +0200

    tennis2: extract logic related with score points into determinePoints() function

--- tennis/javascript-jest/tennis2.js
+++ tennis/javascript-jest/tennis2.js
@@ -20,26 +20,22 @@ const determineEqualScoreOrDeuce = (score, point) => {
   return score
 }
 
+const determinePoints = (score, p1Point, p2Point) => {
+  if (
+    (p1Point > 0 || p2Point > 0) && (p1Point === 0 || p2Point === 0) ||
+    (p1Point > p2Point && p1Point < 4) || (p2Point > p1Point && p2Point < 4)
+  ) {
+    score = `${scores[p1Point]}-${scores[p2Point]}`
+  }
+
+  return score
+}
+
 const getScore = (p1Point, p2Point) => {
   let score = ''
 
   score = p1Point === p2Point && determineEqualScoreOrDeuce(score, p1Point)
-
-  if (p1Point > 0 && p2Point === 0) {
-    score = `${scores[p1Point]}-${scores[p2Point]}`
-  }
-
-  if (p2Point > 0 && p1Point === 0) {
-    score = `${scores[p1Point]}-${scores[p2Point]}`
-  }
-
-  if (p1Point > p2Point && p1Point < 4) {
-    score = `${scores[p1Point]}-${scores[p2Point]}`
-  }
-
-  if (p2Point > p1Point && p2Point < 4) {
-    score = `${scores[p1Point]}-${scores[p2Point]}`
-  }
+  score = determinePoints(score, p1Point, p2Point)
 
   if (p1Point > p2Point && p2Point >= 3) {
     score = 'Advantage player1'
