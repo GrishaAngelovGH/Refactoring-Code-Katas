64926ce964bc364b8917d75d2140cb9f970de219
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-29 09:31:24 +0200

    tennis2: remove unnecessary checks when determine score

--- tennis/javascript-jest/tennis2.js
+++ tennis/javascript-jest/tennis2.js
@@ -3,7 +3,8 @@
 const scores = {
   0: 'Love',
   1: 'Fifteen',
-  2: 'Thirty'
+  2: 'Thirty',
+  3: 'Forty'
 }
 
 const determineEqualScoreOrDeuce = (score, point) => {
@@ -24,66 +25,35 @@ const getScore = (p1Point, p2Point) => {
 
   score = p1Point === p2Point && determineEqualScoreOrDeuce(score, p1Point)
 
-  var P1res
-  var P2res
+  let p1Res
+  let p2Res
+
   if (p1Point > 0 && p2Point === 0) {
-    if (p1Point === 1) {
-      P1res = 'Fifteen'
-    }
-    if (p1Point === 2) {
-      P1res = 'Thirty'
-    }
-    if (p1Point === 3) {
-      P1res = 'Forty'
-    }
+    p1Res = scores[p1Point]
 
-    P2res = 'Love'
-    score = P1res + '-' + P2res
+    p2Res = 'Love'
+    score = p1Res + '-' + p2Res
   }
-  if (p2Point > 0 && p1Point === 0) {
-    if (p2Point === 1) {
-      P2res = 'Fifteen'
-    }
-    if (p2Point === 2) {
-      P2res = 'Thirty'
-    }
-    if (p2Point === 3) {
-      P2res = 'Forty'
-    }
 
-    P1res = 'Love'
-    score = P1res + '-' + P2res
+  if (p2Point > 0 && p1Point === 0) {
+    p2Res = scores[p2Point]
+
+    p1Res = 'Love'
+    score = p1Res + '-' + p2Res
   }
 
   if (p1Point > p2Point && p1Point < 4) {
-    if (p1Point === 2) {
-      P1res = 'Thirty'
-    }
-    if (p1Point === 3) {
-      P1res = 'Forty'
-    }
-    if (p2Point === 1) {
-      P2res = 'Fifteen'
-    }
-    if (p2Point === 2) {
-      P2res = 'Thirty'
-    }
-    score = P1res + '-' + P2res
+    p1Res = scores[p1Point]
+    p2Res = scores[p2Point]
+
+    score = p1Res + '-' + p2Res
   }
+
   if (p2Point > p1Point && p2Point < 4) {
-    if (p2Point === 2) {
-      P2res = 'Thirty'
-    }
-    if (p2Point === 3) {
-      P2res = 'Forty'
-    }
-    if (p1Point === 1) {
-      P1res = 'Fifteen'
-    }
-    if (p1Point === 2) {
-      P1res = 'Thirty'
-    }
-    score = P1res + '-' + P2res
+    p1Res = scores[p1Point]
+    p2Res = scores[p2Point]
+
+    score = p1Res + '-' + p2Res
   }
 
   if (p1Point > p2Point && p2Point >= 3) {
@@ -97,9 +67,11 @@ const getScore = (p1Point, p2Point) => {
   if (p1Point >= 4 && p2Point >= 0 && (p1Point - p2Point) >= 2) {
     score = 'Win for player1'
   }
+
   if (p2Point >= 4 && p1Point >= 0 && (p2Point - p1Point) >= 2) {
     score = 'Win for player2'
   }
+
   return score
 }
 
