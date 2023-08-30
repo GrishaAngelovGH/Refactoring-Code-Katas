554cc27114ede428e2b8973fe8a7c271823b579f
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-30 09:54:31 +0200

    tennis3: extract determineDeuce() and determineAdvantageOrWin() functions

--- tennis/javascript-jest/tennis3.js
+++ tennis/javascript-jest/tennis3.js
@@ -10,22 +10,30 @@ const determineScore = (score, p1Point, p2Point) => {
   return (p1Point === p2Point) ? score + '-All' : score + '-' + points[p2Point]
 }
 
-const getScore = (p1Point, p2Point) => {
-  let score
+const determineDeuce = (p1Point, p2Point) => {
+  if (!((p1Point < 4 && p2Point < 4) && (p1Point + p2Point < 6)) && p1Point === p2Point) {
+    return 'Deuce'
+  }
+
+  return null
+}
+
+const determineAdvantageOrWin = (score, p1Point, p2Point) => {
   let p1Name = 'player1'
   let p2Name = 'player2'
 
-  score = determineScore(score, p1Point, p2Point)
-
   if (!((p1Point < 4 && p2Point < 4) && (p1Point + p2Point < 6))) {
-    if (p1Point === p2Point) {
-      return 'Deuce'
-    }
     score = p1Point > p2Point ? p1Name : p2Name
     return ((p1Point - p2Point) * (p1Point - p2Point) === 1) ? 'Advantage ' + score : 'Win for ' + score
   }
 
-  return score
+  return null
+}
+
+const getScore = (p1Point, p2Point) => {
+  let score
+  score = determineScore(score, p1Point, p2Point)
+  return determineDeuce(p1Point, p2Point) || determineAdvantageOrWin(score, p1Point, p2Point) || score
 }
 
 module.exports = getScore
