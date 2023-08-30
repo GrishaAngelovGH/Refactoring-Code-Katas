245f501a9fc2e2c8c710cefb48e5614ed114ea61
Grisha Angelov <grisha.angelov89@gmail.com>
2023-08-30 10:31:04 +0200

    tennis3: remove unnecessary argument from determineAdvantageOrWin() function

--- tennis/javascript-jest/tennis3.js
+++ tennis/javascript-jest/tennis3.js
@@ -18,13 +18,14 @@ const determineDeuce = (p1Point, p2Point) => {
   return null
 }
 
-const determineAdvantageOrWin = (score, p1Point, p2Point) => {
+const determineAdvantageOrWin = (p1Point, p2Point) => {
   let p1Name = 'player1'
   let p2Name = 'player2'
+  let playerName
 
   if (!((p1Point < 4 && p2Point < 4) && (p1Point + p2Point < 6))) {
-    score = p1Point > p2Point ? p1Name : p2Name
-    return ((p1Point - p2Point) * (p1Point - p2Point) === 1) ? 'Advantage ' + score : 'Win for ' + score
+    playerName = p1Point > p2Point ? p1Name : p2Name
+    return ((p1Point - p2Point) * (p1Point - p2Point) === 1) ? 'Advantage ' + playerName : 'Win for ' + playerName
   }
 
   return null
@@ -33,7 +34,7 @@ const determineAdvantageOrWin = (score, p1Point, p2Point) => {
 const getScore = (p1Point, p2Point) => {
   let score
   score = determineScore(score, p1Point, p2Point)
-  return determineDeuce(p1Point, p2Point) || determineAdvantageOrWin(score, p1Point, p2Point) || score
+  return determineDeuce(p1Point, p2Point) || determineAdvantageOrWin(p1Point, p2Point) || score
 }
 
 module.exports = getScore
