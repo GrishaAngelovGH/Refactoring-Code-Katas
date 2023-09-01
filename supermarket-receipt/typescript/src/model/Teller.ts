f6d496dbb7e62c9ee4ac2e6810f749a76f7206c7
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-01 09:02:19 +0200

    supermarket-receipt: simplify checksOutArticlesFrom() function in Teller

--- supermarket-receipt/typescript/src/model/Teller.ts
+++ supermarket-receipt/typescript/src/model/Teller.ts
@@ -14,17 +14,18 @@ export class Teller {
     this.offers[product.name] = new Offer(offerType, product, argument)
   }
 
-  public checksOutArticlesFrom(theCart: ShoppingCart): Receipt {
+  public checksOutArticlesFrom(cart: ShoppingCart): Receipt {
     const receipt = new Receipt()
-    const productQuantities = theCart.getItems()
-    for (let pq of productQuantities) {
-      let p = pq.product
-      let quantity = pq.quantity
-      let unitPrice = this.catalog.getUnitPrice(p)
-      let price = quantity * unitPrice
-      receipt.addProduct(p, quantity, unitPrice, price)
-    }
-    theCart.handleOffers(receipt, this.offers, this.catalog)
+    const productQuantities = cart.getItems()
+
+    productQuantities.forEach(pq => {
+      const unitPrice = this.catalog.getUnitPrice(pq.product)
+      const price = pq.quantity * unitPrice
+
+      receipt.addProduct(pq.product, pq.quantity, unitPrice, price)
+    })
+
+    cart.handleOffers(receipt, this.offers, this.catalog)
 
     return receipt
   }
