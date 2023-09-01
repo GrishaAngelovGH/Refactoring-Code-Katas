72cb3256fb09b963d691f17d00d9683614b99e72
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-01 09:08:39 +0200

    supermarket-receipt: remove unnecessary quantityAsInt variable from handleOffers() function in ShoppingCart

--- supermarket-receipt/typescript/src/model/ShoppingCart.ts
+++ supermarket-receipt/typescript/src/model/ShoppingCart.ts
@@ -44,36 +44,36 @@ export class ShoppingCart {
       const productQuantity = this._productQuantities[productName]
       const product = productQuantity.product
       const quantity: number = this._productQuantities[productName].quantity
+
       if (offers[productName]) {
         const offer: Offer = offers[productName]
         const unitPrice: number = catalog.getUnitPrice(product)
-        let quantityAsInt = quantity
+
         let discount: Discount | null = null
         let x = 1
+
         if (offer.offerType == SpecialOfferType.ThreeForTwo) {
           x = 3
-
         } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
           x = 2
-          if (quantityAsInt >= 2) {
-            const total = offer.argument * Math.floor(quantityAsInt / x) + quantityAsInt % 2 * unitPrice
+          if (quantity >= 2) {
+            const total = offer.argument * Math.floor(quantity / x) + quantity % 2 * unitPrice
             const discountN = unitPrice * quantity - total
             discount = new Discount(product, '2 for ' + offer.argument, discountN)
           }
-
         } if (offer.offerType == SpecialOfferType.FiveForAmount) {
           x = 5
         }
-        const numberOfXs = Math.floor(quantityAsInt / x)
-        if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
-          const discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice)
+        const numberOfXs = Math.floor(quantity / x)
+        if (offer.offerType == SpecialOfferType.ThreeForTwo && quantity > 2) {
+          const discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantity % 3 * unitPrice)
           discount = new Discount(product, '3 for 2', discountAmount)
         }
         if (offer.offerType == SpecialOfferType.TenPercentDiscount) {
           discount = new Discount(product, offer.argument + '% off', quantity * unitPrice * offer.argument / 100.0)
         }
-        if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
-          const discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice)
+        if (offer.offerType == SpecialOfferType.FiveForAmount && quantity >= 5) {
+          const discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantity % 5 * unitPrice)
           discount = new Discount(product, x + ' for ' + offer.argument, discountTotal)
         }
         if (discount != null)
