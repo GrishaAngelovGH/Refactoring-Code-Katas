f20b1f40f85cd908210f5d01b0fc302bf639f215
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-04 09:12:47 +0200

    theatrical-players: create types lookup object

--- theatrical-players/javascript/src/statement.js
+++ theatrical-players/javascript/src/statement.js
@@ -3,6 +3,25 @@ const format = new Intl.NumberFormat("en-US", {
   minimumFractionDigits: 2
 }).format
 
+const types = {
+  "tragedy": (audience, amount = 40000) => {
+    if (audience > 30) {
+      amount += 1000 * (audience - 30)
+    }
+
+    return amount
+  },
+  "comedy": (audience, amount = 30000) => {
+    if (audience > 20) {
+      amount += 10000 + 500 * (audience - 20)
+    }
+
+    amount += 300 * audience
+
+    return amount
+  }
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0
   let volumeCredits = 0
@@ -10,31 +29,18 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID]
-    let thisAmount = 0
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30)
-        }
-        break
-      case "comedy":
-        thisAmount = 30000
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20)
-        }
-        thisAmount += 300 * perf.audience
-        break
-      default:
-        throw new Error(`unknown type: ${play.type}`)
-    }
+
+    if (!types[play.type]) throw new Error(`unknown type: ${play.type}`)
+
+    const amount = types[play.type](perf.audience)
+
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0)
     // add extra credit for every ten comedy attendees
     if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`
-    totalAmount += thisAmount
+    result += ` ${play.name}: ${format(amount / 100)} (${perf.audience} seats)\n`
+    totalAmount += amount
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`
   result += `You earned ${volumeCredits} credits\n`
