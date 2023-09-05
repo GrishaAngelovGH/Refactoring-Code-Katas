35e752d968ca603a22d5bf74ed12b1b7780a09b6
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-05 10:51:47 +0200

    racing-car: improve readability of Alarm

--- racing-car/javascript/tire-pressure-monitoring-system/alarm.js
+++ racing-car/javascript/tire-pressure-monitoring-system/alarm.js
@@ -1,26 +1,24 @@
-var Sensor = require('./sensor.js');
+const Sensor = require('./sensor.js')
 
-Alarm = function() {
-	this._lowPressureThreshold = 17;
-	this._highPressureThreshold = 21;
-	this._sensor = new Sensor();
-	this._alarmOn = false;
-};
+Alarm = function () {
+  this._lowPressureThreshold = 17
+  this._highPressureThreshold = 21
+  this._sensor = new Sensor()
+  this._alarmOn = false
+}
 
 Alarm.prototype = {
+  check: function () {
+    const psiPressureValue = this._sensor.popNextPressurePsiValue()
 
-	check: function() {
-		var psiPressureValue = this._sensor.popNextPressurePsiValue();
+    if (psiPressureValue < this._lowPressureThreshold || this._highPressureThreshold < psiPressureValue) {
+      this._alarmOn = true
+    }
+  },
 
-		if (psiPressureValue < this._lowPressureThreshold || this._highPressureThreshold < psiPressureValue) {
-			this._alarmOn = true;
-		}
-	},
+  alarmOn: function () {
+    return this._alarmOn
+  }
+}
 
-	alarmOn: function() {
-		return this._alarmOn;
-	}
-
-};
-
-module.exports = Alarm;
+module.exports = Alarm
