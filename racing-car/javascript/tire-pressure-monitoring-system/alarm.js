f52378a5207ac25928a58300e7f53d60caa69d34
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-05 10:53:01 +0200

    racing-car: convert Alarm to class

--- racing-car/javascript/tire-pressure-monitoring-system/alarm.js
+++ racing-car/javascript/tire-pressure-monitoring-system/alarm.js
@@ -1,22 +1,22 @@
 const Sensor = require('./sensor.js')
 
-Alarm = function () {
-  this._lowPressureThreshold = 17
-  this._highPressureThreshold = 21
-  this._sensor = new Sensor()
-  this._alarmOn = false
-}
+class Alarm {
+  constructor() {
+    this._lowPressureThreshold = 17
+    this._highPressureThreshold = 21
+    this._sensor = new Sensor()
+    this._alarmOn = false
+  }
 
-Alarm.prototype = {
-  check: function () {
+  check() {
     const psiPressureValue = this._sensor.popNextPressurePsiValue()
 
     if (psiPressureValue < this._lowPressureThreshold || this._highPressureThreshold < psiPressureValue) {
       this._alarmOn = true
     }
-  },
+  }
 
-  alarmOn: function () {
+  alarmOn() {
     return this._alarmOn
   }
 }
