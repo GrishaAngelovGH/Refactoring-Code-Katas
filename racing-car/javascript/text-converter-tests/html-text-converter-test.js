93cb102909f024207e071086de226b059d583dc3
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-06 09:28:07 +0200

    racing-car: provide unit test to cover that HtmlTextConverter receive filename

--- racing-car/javascript/text-converter-tests/html-text-converter-test.js
+++ racing-car/javascript/text-converter-tests/html-text-converter-test.js
@@ -1,18 +1,13 @@
-/* globals describe, it */
-var chai = require('chai');
-chai.should();
+const chai = require('chai')
+chai.should()
 
-var HtmlTextConverter = require('../text-converter/html-text-converter.js');
+const HtmlTextConverter = require('../text-converter/html-text-converter.js')
 
-describe('Html Converter', function() {
-
-	describe('HtmlTextConverter', function() {
-
-		it('foo', function() {
-			var converter = new HtmlTextConverter('foo');
-			converter.getFilename().should.equal('fixme');
-		});
-
-	});
-
-});
+describe('Html Converter', function () {
+  describe('HtmlTextConverter', function () {
+    it('should get filename', function () {
+      const converter = new HtmlTextConverter('file.txt')
+      converter.getFilename().should.equal('file.txt')
+    })
+  })
+})
