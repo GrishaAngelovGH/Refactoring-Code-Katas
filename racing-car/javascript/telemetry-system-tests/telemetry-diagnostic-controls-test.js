6d400cc636411c51e2646aac461922c744234049
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-08 16:04:27 +0200

    racing-car: provide unit test to cover send diagnostic message and receive a status message response

--- racing-car/javascript/telemetry-system-tests/telemetry-diagnostic-controls-test.js
+++ racing-car/javascript/telemetry-system-tests/telemetry-diagnostic-controls-test.js
@@ -1,16 +1,14 @@
-/* globals describe, it */
-var chai = require('chai');
-chai.should();
+const chai = require('chai')
+chai.should()
 
-var TelemetryDiagnosticControls = require('../telemetry-system/telemetry-diagnostic-controls.js');
+const TelemetryDiagnosticControls = require('../telemetry-system/telemetry-diagnostic-controls.js')
 
-describe('Telemetry System', function() {
-
-	describe('TelemetryDiagnosticControls', function() {
-
-		it('CheckTransmission should send a diagnostic message and receive a status message response', function() {
-		});
-
-	});
-
-});
+describe('Telemetry System', function () {
+  describe('TelemetryDiagnosticControls', function () {
+    it('CheckTransmission should send a diagnostic message and receive a status message response', function () {
+      const telemetryDiagnosticControls = new TelemetryDiagnosticControls()
+      telemetryDiagnosticControls.checkTransmission()
+      telemetryDiagnosticControls.readDiagnosticInfo().should.equal('LAST TX rate................ 100 MBPS\r\nHIGHEST TX rate............. 100 MBPS\r\nLAST RX rate................ 100 MBPS\r\nHIGHEST RX rate............. 100 MBPS\r\nBIT RATE.................... 100000000\r\nWORD LEN.................... 16\r\nWORD/FRAME.................. 511\r\nBITS/FRAME.................. 8192\r\nMODULATION TYPE............. PCM/FM\r\nTX Digital Los.............. 0.75\r\nRX Digital Los.............. 0.10\r\nBEP Test.................... -5\r\nLocal Rtrn Count............ 00\r\nRemote Rtrn Count........... 00')
+    })
+  })
+})
