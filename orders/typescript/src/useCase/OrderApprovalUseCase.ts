09090479248c1e7a574299c6dc2ef5e54e651d96
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-11 14:34:29 +0200

    orders: simplify run() function in OrderApprovalUseCase

--- orders/typescript/src/useCase/OrderApprovalUseCase.ts
+++ orders/typescript/src/useCase/OrderApprovalUseCase.ts
@@ -1,35 +1,36 @@
-import Order from '../domain/Order';
-import { OrderStatus } from '../domain/OrderStatus';
-import OrderRepository from '../repository/OrderRepository';
-import ApprovedOrderCannotBeRejectedException from './ApprovedOrderCannotBeRejectedException';
-import OrderApprovalRequest from './OrderApprovalRequest';
-import RejectedOrderCannotBeApprovedException from './RejectedOrderCannotBeApprovedException';
-import ShippedOrdersCannotBeChangedException from './ShippedOrdersCannotBeChangedException';
+import Order from '../domain/Order'
+import { OrderStatus } from '../domain/OrderStatus'
+import OrderRepository from '../repository/OrderRepository'
+import ApprovedOrderCannotBeRejectedException from './ApprovedOrderCannotBeRejectedException'
+import OrderApprovalRequest from './OrderApprovalRequest'
+import RejectedOrderCannotBeApprovedException from './RejectedOrderCannotBeApprovedException'
+import ShippedOrdersCannotBeChangedException from './ShippedOrdersCannotBeChangedException'
+
+const statuses: { [key in OrderStatus]: () => void } = {
+  [OrderStatus.SHIPPED]: () => {
+    throw new ShippedOrdersCannotBeChangedException()
+  },
+  [OrderStatus.REJECTED]: () => {
+    throw new RejectedOrderCannotBeApprovedException()
+  },
+  [OrderStatus.APPROVED]: () => {
+    throw new ApprovedOrderCannotBeRejectedException()
+  },
+  [OrderStatus.CREATED]: () => { }
+}
 
 class OrderApprovalUseCase {
-  private readonly orderRepository: OrderRepository;
+  private readonly orderRepository: OrderRepository
 
   public constructor(orderRepository: OrderRepository) {
-    this.orderRepository = orderRepository;
+    this.orderRepository = orderRepository
   }
 
   public run(request: OrderApprovalRequest): void {
-    const order: Order = this.orderRepository.getById(request.orderId);
-
-    if (order.status === OrderStatus.SHIPPED) {
-      throw new ShippedOrdersCannotBeChangedException();
-    }
-
-    if (request.isApproved && order.status === OrderStatus.REJECTED) {
-      throw new RejectedOrderCannotBeApprovedException();
-    }
-
-    if (!request.isApproved && order.status === OrderStatus.APPROVED) {
-      throw new ApprovedOrderCannotBeRejectedException();
-    }
-
-    order.updateStatus(request.isApproved ? OrderStatus.APPROVED : OrderStatus.REJECTED);
-    this.orderRepository.save(order);
+    const order: Order = this.orderRepository.getById(request.orderId)
+    statuses[order.status]()
+    order.updateStatus(request.isApproved ? OrderStatus.APPROVED : OrderStatus.REJECTED)
+    this.orderRepository.save(order)
   }
 }
 
