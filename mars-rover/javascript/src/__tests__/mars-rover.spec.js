a2f0d7ef02212e46e2fe3a96270d0ad10ecfe488
Grisha Angelov <grisha.angelov89@gmail.com>
2023-09-14 13:27:56 +0300

    mars-rover: add unit-test to cover default location

--- mars-rover/javascript/src/__tests__/mars-rover.spec.js
+++ mars-rover/javascript/src/__tests__/mars-rover.spec.js
@@ -1,5 +1,9 @@
+const MarsRover = require('../mars-rover')
+
 describe('Mars Rover', function () {
-  test('foo', function () {
-    expect('foo').toBe('')
+  test('should use default location', function () {
+    const marsRover = new MarsRover()
+
+    expect(marsRover.location).toEqual([0, 0])
   })
 })
